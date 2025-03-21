aacc4651b5bc3a56aceb31cf095b6193ce52666d
yonghun16 <yonghun16@naver.com>
2025-03-21 19:32:28 +0900

    state

--- src/components/Squre.jsx
+++ src/components/Squre.jsx
@@ -2,10 +2,19 @@ import React from 'react'
 import './Squre.css'
 
 export default class Squre extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: null,
+    }
+  }
+
   render() {
     return (
-      <button className="squre">
-        {this.props.value}
+      <button className="squre" onClick={()=>{
+        this.setState({value: 'X'});
+      }}>
+        {this.state.value}
       </button>
     )
   }
