a11c487330df66258fe5bb1266ed74de576ab79f
yonghun16 <yonghun16@naver.com>
2025-03-22 05:46:56 +0900

    update

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,14 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+
+  build: {
+    minify: 'terser',      // 기본 minifier인 'esbuild' 대신 'terser' 사용
+    terserOptions: {
+      format: {
+        comments: false,   // 모든 주석 제거
+      },
+    },
+  },
 })
+
