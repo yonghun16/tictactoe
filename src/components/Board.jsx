b05708533dfc4efd2751e9195333c83f96f2feb2
yonghun16 <yonghun16@naver.com>
2025-03-22 01:36:43 +0900

    승리 추가

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,33 +1,80 @@
-import React, { Component } from 'react'
+import React, { useState } from 'react'
 import Squre from './Squre'
 import './Board.css'
 
-export default class Board extends Component {
-  renderSquare(i) {
-    return <Squre value={i}/>  
+const Board = () => {
+  const [squares, setSquares] = useState(Array(9).fill(null));
+  const [xIsNext, setXIsNext] = useState(true);
+
+  const calculateWinner = (squares) => {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+        return squares[a];
+      }
+    }
+    return null;
   }
 
-  render() {
-    return (
-      <div>
-        <div className="status">Next player: X, O </div>
-        <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
-        </div>
-      </div>
-    )
+  const winner = calculateWinner(squares);
+  let status;
+  if (winner) {
+    status = `Winner: ${winner}`;
   }
+  else {
+    status = `Next player: ${xIsNext ? 'X' : 'O'}`;
+  }
+
+  const handleClick = (i) => {
+    const newSquares = squares.slice();
+
+    if (calculateWinner(squares) || squares[i]) {
+      return;
+    }
+
+    newSquares[i] = xIsNext ? 'X' : 'O';
+    setSquares(newSquares);
+    setXIsNext(!xIsNext);
+  }
+
+  const renderSquare = (i) => {
+    return <Squre
+      value={squares[i]}
+      onClick={() => handleClick(i)}
+    />
+  }
+
+  return (
+    <div>
+      <div className="status"> {status} </div>
+      <div className="board-row">
+        {renderSquare(0)}
+        {renderSquare(1)}
+        {renderSquare(2)}
+      </div>
+      <div className="board-row">
+        {renderSquare(3)}
+        {renderSquare(4)}
+        {renderSquare(5)}
+      </div>
+      <div className="board-row">
+        {renderSquare(6)}
+        {renderSquare(7)}
+        {renderSquare(8)}
+      </div>
+    </div>
+  )
 }
+
+export default Board
 
--- src/components/Squre.jsx
+++ src/components/Squre.jsx
@@ -1,21 +1,13 @@
 import React from 'react'
 import './Squre.css'
 
-export default class Squre extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      value: null,
-    }
-  }
-
-  render() {
-    return (
-      <button className="squre" onClick={()=>{
-        this.setState({value: 'X'});
-      }}>
-        {this.state.value}
-      </button>
-    )
-  }
+const Squre = ({onClick, value}) => {
+  return (
+    <button className="squre"
+      onClick={onClick}>
+      {value}
+    </button>
+  )
 }
+
+export default Squre
